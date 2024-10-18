fd044881aab7d55fc86b954d7d9b596147c633f9
upendrabhat <bhatupendra1234@gmail.com>
2024-10-18 11:20:00 +0530

    first commit

---
+++ day2devops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace day2devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
