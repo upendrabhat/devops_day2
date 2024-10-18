6148fda8f5abcb286a3329b44e0b1733134fa81b
upendrabhat <bhatupendra1234@gmail.com>
2024-10-18 11:38:58 +0530

    secound change

--- day2devops/obj/Debug/net8.0/day2devops.AssemblyInfo.cs
+++ day2devops/obj/Debug/net8.0/day2devops.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day2devops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd044881aab7d55fc86b954d7d9b596147c633f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("day2devops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day2devops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
